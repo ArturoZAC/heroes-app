873aa9e0e27d1b76793ffb249a18ed1948e54495
4z4c <arturoyz2105@gmail.com>
2025-08-24 10:37:42 -0500

    C3HEAPP

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,4 +1,14 @@
+import { CustomJombotron } from "@/components/custom/CustomJombotron";
 
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CustomJombotron
+        title="Búsqueda de SuperHeroes"
+        description="Descubre, explora y administra super héroes y villanos"
+      />
+    </>
+  )
 };
+
+export default SearchPage;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,7 +4,10 @@ import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
+import { lazy } from "react";
+
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
 
 export const appRouter = createBrowserRouter([
   {
